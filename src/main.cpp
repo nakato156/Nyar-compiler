acab09f290701ebbeaa6052270d5f35e73af3951
Giorgio6846 <6852733+Giorgio6846@users.noreply.github.com>
2024-09-28 21:23:41 -0500

    Minor changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,56 @@
 #include "antlr4-runtime.h"
 
-int main(){
+#include "./lib/NyarLexer.h"
+#include "./lib/NyarParser.h"
+#include "./visitor/NyarVisitor.h"
 
+using namespace std;
+using namespace antlr4;
+
+int abrirArchivo(int argc, char *argv[]){
+
+    if (argc <= 1)
+        return -1;
+
+    string linea;
+    vector<string> params, path_files;
+
+    for (int i = 1; i < argc; i++) {
+        string opcion = argv[i];
+        if (opcion.length() > 0 && opcion[0] != '-') {
+            path_files.push_back(opcion);
+            continue;
+        }
+        params.push_back(opcion.substr(1));
+    }
+
+    if (!path_files.empty()) {
+        ifstream file(path_files[0]);
+        if (!file.good()) {
+            printf("El archivo no existe, revise la ruta.\n");
+            return EXIT_FAILURE;
+        }
+
+        ANTLRInputStream input(file);
+        NyarLexer lexer(&input);
+        CommonTokenStream tokens(&lexer);
+        NyarParser parser(&tokens); 
+        NyarParser::ProgramContext* tree = parser.program();
+
+
+        NyarVisitor eval;
+        eval.visitProgram(tree);
+        eval.compile();
+        
+        cout << tree->toStringTree(&parser) << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]){
+    int result = abrirArchivo(argc, argv);
+    cout << "oki doki" << endl;
+    if (result != -1) {
+        return result;
+    }
 }
